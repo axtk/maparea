8f6cec8fdb5b83c4f99a9d3c36cea76caaae5cd4
axtk <axtkx@inbox.ru>
2026-01-19 23:00:51 +0300

    lint

--- src/utils/fitBounds.ts
+++ src/utils/fitBounds.ts
@@ -1,23 +1,31 @@
-import { MapArea } from "../MapArea/index.ts";
-import { GeoBounds } from "../types/GeoBounds.ts";
+import type { MapArea } from "../MapArea/index.ts";
+import type { GeoBounds } from "../types/GeoBounds.ts";
 
 const { abs, log2, min, floor } = Math;
 
 export function fitBounds(map: MapArea, bounds: GeoBounds) {
-  let { box: { w, h }, center: [lat, lon], zoom } = map;
+  let {
+    box: { w, h },
+    center: [lat, lon],
+    zoom,
+  } = map;
   let { minLat, maxLat, minLon, maxLon } = bounds;
 
   let dzx = 0;
   let dzy = 0;
 
   if (minLon !== undefined && maxLon !== undefined) {
-    let dx = abs(map.toPixelCoords(lat, maxLon)[0] - map.toPixelCoords(lat, minLon)[0]);
+    let dx = abs(
+      map.toPixelCoords(lat, maxLon)[0] - map.toPixelCoords(lat, minLon)[0],
+    );
 
     if (dx !== 0) dzx = log2(w / dx);
   }
 
   if (minLat !== undefined && maxLat !== undefined) {
-    let dy = abs(map.toPixelCoords(minLat, lon)[1] - map.toPixelCoords(maxLat, lon)[1]);
+    let dy = abs(
+      map.toPixelCoords(minLat, lon)[1] - map.toPixelCoords(maxLat, lon)[1],
+    );
 
     if (dy !== 0) dzy = log2(h / dy);
   }
