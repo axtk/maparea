80f0e98c8f226ff355a841c52937bf6b42177fa1
axtk <axtkx@inbox.ru>
2026-09-15 17:09:15 +0300

    check layer errors in composite image

--- src/utils/getCompositeImage.ts
+++ src/utils/getCompositeImage.ts
@@ -1,6 +1,12 @@
 import type { MapArea } from "../MapArea/index.ts";
 import { setSize } from "./canvas/setSize.ts";
 
+async function getBlob(canvas: HTMLCanvasElement, type?: string, quality?: number) {
+  return new Promise<Blob | null>((resolve) => {
+    canvas.toBlob((blob) => resolve(blob), type, quality);
+  });
+}
+
 export async function getCompositeImage(
   map: MapArea,
   type?: string,
@@ -14,10 +20,15 @@ export async function getCompositeImage(
 
   for (let layer of map.container.querySelectorAll<HTMLCanvasElement>(
     "canvas.layer",
-  ))
-    ctx.drawImage(layer, 0, 0);
+  )) {
+    try {
+      // Pick only the layers that can be converted to Blob without errors
+      await getBlob(layer, type, quality);
+      ctx.drawImage(layer, 0, 0);
+    } catch (e) {
+      console.warn(`Error occurred while exporting '${layer.dataset.id}' layer.\n${e}`);
+    }
+  }
 
-  return new Promise<Blob | null>((resolve) => {
-    canvas.toBlob((blob) => resolve(blob), type, quality);
-  });
+  return getBlob(canvas, type, quality);
 }
